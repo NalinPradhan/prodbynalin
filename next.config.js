a5b0a7da83a2e2a015018244fcd145d3c8256dee
Nalin <pnalin63@gmail.com>
2025-03-02 23:49:04 +0530

    3rd try deploying

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: false,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  output: "standalone",
+};
+
+module.exports = nextConfig;
